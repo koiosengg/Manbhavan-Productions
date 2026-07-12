6592f9c2a72736d57e55339be7eb40833007de1a
Panth1823 <panthshah242@gmail.com>
2026-07-12 15:57:11 +0530

    logo update and text line height update

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/Home/Contact/Logo.webp";
+import Logo from "../../assets/Home/Contact/Logo.png";
 
 function Contact() {
   return (
